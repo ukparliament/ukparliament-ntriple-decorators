396a4f19231962e9a40a94ec8d66a478bfcb054a
Callum Neve-Jones <c.nevejones@gmail.com>
2017-09-12 13:58:17 +0100

    Added additional date decorators to FormalBody
    * Added new decorators
    * Added specs
    * Bumped version

--- lib/parliament/grom/decorator/version.rb
+++ lib/parliament/grom/decorator/version.rb
@@ -1,7 +1,7 @@
 module Parliament
   module Grom
     module Decorator
-      VERSION = '0.5.0'.freeze
+      VERSION = '0.6.0'.freeze
     end
   end
 end
--- spec/parliament/grom/decorator/formal_body_spec.rb
+++ spec/parliament/grom/decorator/formal_body_spec.rb
@@ -22,4 +22,49 @@ describe Parliament::Grom::Decorator::FormalBody, vcr: true do
       end
     end
   end
+
+
+  context '#start_date' do
+    context 'Grom::Node has a start date' do
+      it 'will return correct start date' do
+        expect(@formal_body_nodes[0].start_date).to eq(DateTime.parse('1981-11-12T00:00:00+00:00'))
+        expect(@formal_body_nodes[0].start_date.class).to eq(DateTime)
+      end
+    end
+
+    context 'Grom::Node has no start date' do
+      it 'will return nil' do
+        expect(@formal_body_nodes[1].start_date).to eq(nil)
+      end
+    end
+  end
+
+  context '#end_date' do
+    context 'Grom::Node has an end date' do
+      it 'will return correct end date' do
+        expect(@formal_body_nodes[0].end_date).to eq(DateTime.parse('1991-11-12T00:00:00+00:00'))
+        expect(@formal_body_nodes[0].end_date.class).to eq(DateTime)
+      end
+    end
+
+    context 'Grom::Node has no end date' do
+      it 'will return nil' do
+        expect(@formal_body_nodes[1].end_date).to eq(nil)
+      end
+    end
+  end
+
+  context '#current?' do
+    context 'is current' do
+      it 'will return true' do
+        expect(@formal_body_nodes[1].current?).to eq(true)
+      end
+    end
+
+    context 'is not current' do
+      it 'will return false' do
+        expect(@formal_body_nodes[1].current?).to eq(false)
+      end
+    end
+  end
 end
