2b866a576579fa4fecd96ed7359c553a6b805b1c
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2018-02-22 13:50:41 +0000

    GTP - Changes to Collection and WebArticle decorators
    * Add additional Collection decorators
    * Add Collection decorator tests
    * Amend WebArticle decorator for collections, following change in predicates
    * Amend WebArticle decorator tests

--- lib/parliament/grom/decorator/collection.rb
+++ lib/parliament/grom/decorator/collection.rb
@@ -5,11 +5,39 @@ module Parliament
       # Collection Grom::Node is a Contentful object associated with a web article (a WebArticle has an Audience node)
       # Collection Grom::Node is not modelled in the data service, hence type 'http://example..' and not 'https://id.parliament.uk/schema'
       module Collection
-        # Alias name with fallback.
+        # Alias collectionName with fallback.
         #
-        # @return [String, String] the title of the Grom::Node or an empty string.
-        def collection_name
-          respond_to?(:name) ? name : ''
+        # @return [String, String] the name of the Grom::Node or an empty string.
+        def name
+          respond_to?(:collectionName) ? collectionName : ''
+        end
+
+        # Alias collectionDescription with fallback.
+        #
+        # @return [String, String] the description of the Grom::Node or an empty string.
+        def description
+          respond_to?(:collectionDescription) ? collectionDescription : ''
+        end
+
+        # Alias collectionHasArticle with fallback.
+        #
+        # @return [Array, Array] an array of Articles related to the Grom::Node or an empty Array.
+        def articles
+          respond_to?(:collectionHasArticle) ? collectionHasArticle : []
+        end
+
+        # Alias collectionHasSubcollection with fallback.
+        #
+        # @return [Array, Array] an array of Subcollections related to the Grom::Node or an empty Array.
+        def subcollections
+          respond_to?(:collectionHasSubcollection) ? collectionHasSubcollection : []
+        end
+
+        # Alias collectionHasParent with fallback.
+        #
+        # @return [Array, Array] an array of Subcollections related to the Grom::Node or an empty Array.
+        def parents
+          respond_to?(:collectionHasParent) ? collectionHasParent : []
         end
       end
     end
--- spec/parliament/grom/decorator/collection_spec.rb
+++ spec/parliament/grom/decorator/collection_spec.rb
@@ -7,23 +7,79 @@ describe Parliament::Grom::Decorator::Collection, vcr: true do
       base_url: 'http://localhost:3030/api/v1',
       builder: Parliament::Builder::NTripleResponseBuilder,
       decorators: Parliament::Grom::Decorator).collection_by_id.get
-    @collection = (response.filter('http://example.com/content/Collection')).first
+    @collection = (response.filter('https://id.parliament.uk/schema/Collection')).first
   end
 
-  describe '#collection_name' do
+  describe '#name' do
     context 'Grom::Node has all the required objects' do
-      it 'confirms that the type for this Grom::Node object is Article' do
-        expect(@collection.type).to eq('http://example.com/content/Collection')
+      it 'confirms that the type for this Grom::Node object is Collection' do
+        expect(@collection.type).to eq('https://id.parliament.uk/schema/Collection')
       end
 
-      it 'returns the title of the article the Grom::Node object' do
-        expect(@collection.collection_name).to eq('collection 1')
+      it 'returns the name of the article the Grom::Node object' do
+        expect(@collection.name).to eq('Questions')
       end
     end
 
-    context 'Grom::Node does not have a title' do
+    context 'Grom::Node does not have a name' do
       it 'returns an empty string' do
-        expect(@collection.collection_name).to eq('')
+        expect(@collection.name).to eq('')
+      end
+    end
+  end
+
+  describe '#description' do
+    context 'Grom::Node has all the required objects' do
+      it 'returns the description of the article the Grom::Node object' do
+        expect(@collection.description).to eq('You can ask a Minister an oral question in person or a written question. Questions are submitted via the Table Office.')
+      end
+    end
+
+    context 'Grom::Node does not have a description' do
+      it 'returns an empty string' do
+        expect(@collection.description).to eq('')
+      end
+    end
+  end
+
+  describe '#articles' do
+    context 'Grom::Node has all the required objects' do
+      it 'returns an array of WebArticle Grom::Nodes' do
+        expect(@collection.articles.first.type).to eq('https://id.parliament.uk/schema/WebArticle')
+      end
+    end
+
+    context 'Grom::Node does not have articles' do
+      it 'returns an empty array' do
+        expect(@collection.articles).to eq([])
+      end
+    end
+  end
+
+  describe '#subcollections' do
+    context 'Grom::Node has all the required objects' do
+      it 'returns an array of Collection Grom::Nodes' do
+        expect(@collection.subcollections.first.type).to eq('https://id.parliament.uk/schema/Collection')
+      end
+    end
+
+    context 'Grom::Node does not have subcollections' do
+      it 'returns an empty array' do
+        expect(@collection.subcollections).to eq([])
+      end
+    end
+  end
+
+  describe '#parents' do
+    context 'Grom::Node has all the required objects' do
+      it 'returns an array of Collection Grom::Nodes' do
+        expect(@collection.parents.first.type).to eq('https://id.parliament.uk/schema/Collection')
+      end
+    end
+
+    context 'Grom::Node does not have parent collections' do
+      it 'returns an empty array' do
+        expect(@collection.parents).to eq([])
       end
     end
   end
