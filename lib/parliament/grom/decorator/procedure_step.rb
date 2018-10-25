ce2befecd05f1ebc0ff510cebd1e7c965e6985a2
Matt Rayner <m@rayner.io>
2018-10-25 14:10:49 +0100

    Remove unnecessary decorators from ProcedureStep

--- lib/parliament/grom/decorator/procedure_step.rb
+++ lib/parliament/grom/decorator/procedure_step.rb
@@ -3,19 +3,6 @@ module Parliament
     module Decorator
       # Decorator namespace for Grom::Node instances with type: https://id.parliament.uk/schema/ProcedureStep
       module ProcedureStep
-        # Alias procedureStepHasBusinessItem with fallback.
-        #
-        # @return [Array, Array] an array of BusinessItem Grom::Nodes or an empty array.
-        def business_items
-          @business_items ||= respond_to?(:procedureStepHasBusinessItem) ? procedureStepHasBusinessItem : []
-        end
-
-        # Alias procedureStepHasHouse with fallback.
-        #
-        # @return [Array, Array] an array of House Grom::Nodes or an empty array.
-        def houses
-          @houses ||= respond_to?(:procedureStepHasHouse) ? procedureStepHasHouse : []
-        end
       end
     end
   end
